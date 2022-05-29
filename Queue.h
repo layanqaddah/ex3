7c7ba6dc4c3716d1849bf58dbcb96bf03be6c4a5
omar1a4 <103324477+omar1a4@users.noreply.github.com>
2022-05-29 20:32:44 +0300

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -5,56 +5,100 @@
 #ifndef ASSIGNMENT3_QUEUE_H
 #define ASSIGNMENT3_QUEUE_H
 
-template <class T>
-class Node{
-public:
-    T m_nodeValue;
-    Node m_nextNode;
-
-    explicit Node(const T& nodeValue):m_nodeValue(nodeValue),m_nextNode(nullptr){};
-    ~Node()=default;
-    Node(const Node& nodeValue)=delete;
-    Node& operator=(const Node& nodeValue)=delete;
-};
-
 //typedef int S;
-template <class S>
-class Queue{
+template <class T>
+class Queue
+{
 public:
     Queue():m_frontNode(nullptr), m_backNode(nullptr), m_queueSize(0){};
     Queue(const Queue& queue);
     Queue& operator=(const Queue& queue);
     ~Queue();
 
-    void pushBack(const S& newNodeData);
-    S& front();
+    void pushBack(const T& newNodeData);
+    T& front();
     void popFront();
     int size() const;
 
     class Iterator;
-    Iterator begin();
-    Iterator end();
+    Iterator begin()
+    {
+        return Iterator(m_frontNode);
+    };
+    Iterator end()
+    {
+        return Iterator(nullptr);
+    };
 
 private:
-    Node<S> *m_frontNode;
-    Node<S> *m_backNode;
+    class Node{
+    public:
+        T m_nodeValue;
+        Node *m_nextNode;
+
+        explicit Node(const T& nodeValue):m_nodeValue(nodeValue),m_nextNode(nullptr){};
+        ~Node()=default;
+        Node(const Node& nodeValue)=delete;
+        Node& operator=(const Node& nodeValue)=delete;
+    };
+
+    Node *m_frontNode;
+    Node *m_backNode;
     int m_queueSize;
 };
 
-template <class S>
-Queue<S>::Queue(const Queue<S>& queue):m_frontNode(new Node<S>(queue->m_frontNode)),
-                                       m_backNode(new Node<S>(queue.m_backNode)), m_queueSize(queue.m_queueSize)
+template <class T>
+Queue<T>::Queue(const Queue<T>& queue):m_frontNode(new Node(queue->m_frontNode)),
+                                       m_backNode(new Node(queue.m_backNode)), m_queueSize(queue.m_queueSize)
 {
+                                            
 }
 
-template <class S>
-class Queue<S>::Iterator
+
+
+template <class T>
+class Queue<T>::Iterator
 {
 private:
-    Node<S> *current_node;
+    Node *current_node;
 public:
-    explicit Iterator():
+    Iterator():current_node(nullptr){};
+    ~Iterator()=default;
+    Iterator(const Iterator&)=delete;
+    Iterator operator=(const Iterator&)=delete;
+    Iterator& operator++();
+    Iterator& operator++(int);
+    bool operator!=(const Iterator& iterator)const;
+    T& operator*();
+
 };
 
+template <class T>
+typename Queue<T>::Iterator& Queue<T>::Iterator::operator++()
+{
+    current_node = current_node->m_nextNode;
+    return *this;
+}
+
+template <class T>
+typename Queue<T>::Iterator& Queue<T>::Iterator::operator++(int)
+{
+    Queue<T>::Iterator *tmp=this;
+    ++*this;
+    return tmp;
+}
+
+
+template <class T>
+bool Queue<T>::Iterator::operator!=(const Iterator& iterator)const
+{
+    return !(current_node==iterator.current_node);
+}
+
+template <class T>
+T& Queue<T>::Iterator::operator*()
+{
+    return current_node->m_nodeValue;
+}
 
 #endif //ASSIGNMENT3_QUEUE_H
